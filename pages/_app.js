083c9b5536642a2ddd4523c8c31a5b8705c1ddff
Jain-Daksh <daksh@gmail.com>
2023-03-10 11:21:39 +0530

    added pages

---
+++ pages/_app.js
@@ -0,0 +1,19 @@
+
+//import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import '../design/home.css'
+import '../design/navbar.css'
+import '../design/whyToRegister.css'
+import '../design/discover.css'
+import '../design/footer.css'
+import '../design/join.css'
+import '../design/contactus.css'
+function MyApp({ Component, pageProps }) {
+  return(
+   
+      <Component {...pageProps} />
+   
+  ) 
+}
+
+export default MyApp
---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import Nav from '../components/Nav'
+import HomePage from '../components/Home'
+import About from '../components/AboutUs'
+import Discover from '../components/Discover'
+import WhyToRegister from '../components/WhyToRegister'
+import StudentCare from '../components/StudentCare'
+import Courses from '../components/Courses'
+import Join from '../components/Join'
+import Images from '../components/Images'
+import Contact from '../components/ContactUs'
+import Footer from '../components/Footer'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Lakshya Academy</title>
+      </Head>
+      <Nav />
+      <HomePage />
+      <About />
+      <Discover />
+      <WhyToRegister />
+      <Courses />
+      <Join></Join>
+      <Images />
+      <Contact />
+      <Footer></Footer>
+    </div>
+  )
+}
